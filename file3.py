5085fc38bdf383a5d3e5026fa9bc09a2637b447c
sahith8367 <sahith8367@gmail.com>
2025-04-26 15:14:12 +0530

    6th commit

--- file3.py
+++ file3.py
@@ -25,3 +25,9 @@ def star(a):
         for j in range(20):
             print(i)
 print(star(10))
+
+def star(a):
+    for i in range(20):
+        for j in range(20):
+            print(i)
+print(star(10))
