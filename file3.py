957ed46e4596dce72a73121af4d3cb60f286ee83
sahith8367 <165365461+sahith8367@users.noreply.github.com>
2025-04-26 14:36:04 +0530

    Updated file3.py
    added decrement function

--- file3.py
+++ file3.py
@@ -4,3 +4,10 @@ def increment(a):
         print(i+1)
 
 print(increment(20))
+# another function 
+
+def decrement(b):
+    for i in range(b):
+        a = b-i
+        print(a)
+print(decrement(20))
