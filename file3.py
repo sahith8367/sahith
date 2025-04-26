805d09cf93b99e0c9d99c751f9085b1263ca1a92
sahith8367 <sahith8367@gmail.com>
2025-04-26 14:47:28 +0530

    4th commit

--- file3.py
+++ file3.py
@@ -11,3 +11,11 @@ def decrement(b):
         a = b-i
         print(a)
 print(decrement(20))
+
+#another function
+
+def star(a):
+    for i in range(20):
+        for j in range(20):
+            print(i)
+print(star(10))
